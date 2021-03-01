7f42c3c2f65cfc32ca745e43e4573d1c11ec043e
Aliaksei Kakhavets <aliaksei_kakhavets@epam.com>
2021-03-01 20:25:37 +0300

    lab5 v1.0

---
+++ handlers/pulls.py
@@ -0,0 +1,25 @@
+import requests
+
+passw = ('user', 'pass')
+
+
+def get_pulls(state):
+
+    if state in ('open', 'closed'):
+        response = requests.get(f'https://api.github.com/repos/alenaPy/devops_lab/pulls?state={state}&per_page=100',
+                                auth=passwd.passw)
+        pulls_list = response.json()
+    elif state in ('needs work', 'accepted'):
+        response = requests.get('https://api.github.com/search/issues?q=is:pr%20'
+                                f'label:\"{state}\"%20repo:alenaPy/devops_lab&per_page=100',
+                                auth=passwd.passw)
+        pulls_list = response.json()['items']
+    else:
+        response = requests.get('https://api.github.com/repos/alenaPy/devops_lab/pulls?state=all&per_page=100',
+                                auth=passwd.passw)
+        pulls_list = response.json()
+
+    ans = []
+    for i in pulls_list:
+        ans.append({'num': i['number'], 'title': i['title'], 'link': i['html_url']})
+    return ans
